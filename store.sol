a553a53180d89bf507409217427199e53428ce8b
bloodstalker <thabogre@gmail.com>
2017-09-08 17:42:29 +0430

    init commit

---
+++ store.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.0;
+
+contract SimpleStorage {
+  uint StoreData;
+  address public minter;
+  mapping (address => uint) public balances;
+
+  event Sent(address from, address to, uint amount);
+
+  function SimpleStorage(address minter, uint data) {
+    StoreData = data;
+    minter = msg.sender;
+  }
+
+  function mint(address receiver, uint amount) {
+    if (msg.sender != minter) return;
+    balances[receiver]+= amount;
+  }
+
+  function send(address receiver, uint amount) {
+    if (balances[msg.sender] < amount) return;
+    balances[msg.sender] -= amount;
+    balances[receiver] += amount;
+    Sent(msg.sender, receiver, amount);
+  }
+
+  function set(uint x) {
+    StoreData = x;
+  }
+
+  function get() constant returns (uint) {
+    return StoreData;
+  }
+}
